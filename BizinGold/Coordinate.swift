3a2d7a4bf80a0e3192486dff5835c004890341d0
Guilherme <guiaszul@gmail.com>
2020-02-18 15:19:08 -0300

    MovePieces

---
+++ BizinGold/Coordinate.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinate.swift
+//  BizinGold
+//
+//  Created by Guilherme Araujo on 17/02/20.
+//  Copyright © 2020 Guilherme Araujo. All rights reserved.
+//
+
+import Foundation
+
+struct Coordinate {
+    let row: Int
+    let column: Int
+}
--- BizinGold/GameScene.swift
+++ BizinGold/GameScene.swift
@@ -9,19 +9,60 @@
 import SpriteKit
 import GameplayKit
 
+enum pieceColor: String {
+    case blue
+    case green
+}
+
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
+
     private var spinnyNode : SKShapeNode?
+    var board: SKTileMapNode!
+    var bluePoints: [SKSpriteNode]!
+    var greenPoints: [SKSpriteNode]!
+    var selectedPin = SKSpriteNode()
+    weak var viewController: GameViewController?
+    
+    var pieceMatrix = (0,0)
+    var touchCount = 0
+    var totalMoves = -1
+    
+    var possibleBlueMoves: [(Int, Int)] = []
+    var possibleGreenMoves: [(Int, Int)] = []
+    
+    let possibleOrientations = [(1,1), (1,-1), (0,2), (0,-2), (-1,1),(-1,-1)]
+    let piecesAround = [(0,1), (0,-1), (1,0), (-1,0)]
+    var count: [SKNode] = []
+    
+    var blueInitialPosition: [CGPoint] = []
+    var greenInitialPosition: [CGPoint] = []
+    
+    var player: String = ""
+    var username: String = ""
+    var originalPin: SKTexture?
+
+   // var playerTurn: pieceColor = .blue
+    
+    var previousMove: Coordinate!
+    
+    
+    
     
     override func didMove(to view: SKView) {
+        board = childNode(withName: "bizingoBoard") as? SKTileMapNode
+        bluePoints = board.children
+            .filter { ($0.name == "isBlue" || $0.name == "blueCaptain") }
+            .map { $0 as! SKSpriteNode }
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        greenPoints = board.children
+            .filter { ($0.name == "isGreen" || $0.name == "greenCaptain") }
+            .map { $0 as! SKSpriteNode }
+        
+        
+        blueInitialPosition = bluePoints.map { $0.position }
+        greenInitialPosition = greenPoints.map { $0.position }
+        
+        setupGameBoard()
         
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
@@ -38,40 +79,127 @@ class GameScene: SKScene {
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+    private func setupGameBoard() {
+        let columns = board.numberOfColumns
+        let rows = board.numberOfRows
+        
+        for col in 0..<columns {
+            for row in 0..<rows {
+                let tile = board.tileGroup(atColumn: col, row: row)
+                if let tileGroupName = tile?.name,
+                    tileGroupName == "Up Tiles" {
+                    let tileDefinition = board.tileDefinition(atColumn: col, row: row)
+                    tileDefinition?.userData = NSMutableDictionary()
+                    tileDefinition?.userData?.setValue(true, forKey: "boardup")
+                    possibleBlueMoves.append((col, row))
+                }
+                if let tileGroupName = tile?.name,
+                    tileGroupName == "Down Tiles" {
+                    let tileDefinition = board.tileDefinition(atColumn: col, row: row)
+                    tileDefinition?.userData = NSMutableDictionary()
+                    tileDefinition?.userData?.setValue(true, forKey: "boardDown")
+                    possibleGreenMoves.append((col,row))
+                }
+            }
         }
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+    private func movePiece(atPos pos: CGPoint) {
+        let boardPos = self.convert(pos, to: board)
+        let col = board.tileColumnIndex(fromPosition: boardPos)
+        let row = board.tileRowIndex(fromPosition: boardPos)
+        let tileNode = board.nodes(at: boardPos).first
+        let clickTexture: SKTexture = SKTexture.init(imageNamed: "selectedPin")
+        
+        
+            if touchCount < 1 {
+                if checkIfIsPiece([tileNode ?? SKNode()]) {
+                    
+                    selectedPin = tileNode as! SKSpriteNode
+                    selectedPin.removeAction(forKey: "drop")
+                    originalPin = self.returnTextureFrom(selectedPin)
+                    selectedPin.run(SKAction.setTexture(clickTexture), withKey: "pickup")
+//                    selectedPin.run(SKAction.scale(to: 10, duration: 0.25), withKey: "pickup")
+                    pieceMatrix = (row,col)
+                    touchCount += 1
+                }
+            } else {
+                movePieceTo(piece: selectedPin, pieceMatrix: pieceMatrix, col: col, row: row)
+                selectedPin.removeAction(forKey: "pickup")
+                selectedPin.run(SKAction.setTexture(originalPin!), withKey: "drop")
+                touchCount += 1
+
+            }
+            
+            if touchCount == 2 {
+                touchCount = 0
+                selectedPin = SKSpriteNode()
+            }
         }
+
+    
+    private func checkIfIsPiece(_ nodes: [SKNode]) -> Bool {
+        let pieceCount = nodes.filter { $0.userData?["isPiece"] != nil }.count
+        return pieceCount > 0 ? true : false
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+    func returnTextureFrom(_ node: SKSpriteNode) -> SKTexture{
+        var texture: SKTexture?
+        if node.name == "isBlue"{
+            texture = SKTexture.init(imageNamed: "pin1")
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        else if node.name == "blueCaptain" {
+            texture = SKTexture.init(imageNamed: "captain1")
+        }
+        
+        else if node.name == "isGreen" {
+            texture = SKTexture.init(imageNamed: "pin2")
+        }
+        
+        else if node.name == "greenCaptain"{
+            texture = SKTexture.init(imageNamed: "captain2")
+        }
+        return texture!
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    
+    
+    //MARK: A FUNÇÃO
+    func movePieceTo(piece: SKSpriteNode,pieceMatrix: (Int, Int), col: Int, row: Int) {
+        let boardTileCenter = board.centerOfTile(atColumn: col, row: row)
+        let offset: CGFloat = -20
+        var position = CGPoint(x: boardTileCenter.x, y: boardTileCenter.y)
+        
+        //corrigindo centro da peça
+        if piece.name == "isBlue" || piece.name == "blueCaptain" {
+            position.y += offset
+        } else {
+            position.y -= offset
+        }
+        
+        //impedindo sobreposição de peça
+        
+        let rowOrientation = pieceMatrix.0 - row
+        let colOrientation = pieceMatrix.1 - col
+        
+        let tuple = (rowOrientation, colOrientation)
+        
+        possibleOrientations.forEach { (orientation) in
+            if orientation.0 == tuple.0 && orientation.1 == tuple.1 {
+                if board.nodes(at: boardTileCenter).count == 0 {
+                    piece.run(SKAction.move(to: position, duration: 0.5)) {
+                        
+                    }
+                }
+            }
+        }
+    }
+  
+   
+    
+    func touchUp(atPoint pos : CGPoint) {
+        movePiece(atPos: pos)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
